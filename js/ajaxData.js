b399d956fcbfab673520d8a7a1166ab96a7f2242
sshrestha <sshrestha@medforce.net>
2015-07-08 15:30:56 -0400

    Student added

--- js/ajaxData.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('ajaxApp', []);
-
-app.controller('StudentController', function ($scope,$http) {
-		var url="data.txt";
-   		$http.get(url).success( function(response) {
-                           $scope.students = response;
-                        });
-
-});
--- js/main.js
+++ js/main.js
@@ -1,13 +1,22 @@
 
-var app = angular.module('myApp', ['myApp.studentlist']);
+var app = angular.module('myApp', []);
 
+app.run(function($rootScope) {
+  $rootScope.name = "Ari Lerner";
+});
 
-app.controller('greetingController', function($scope) {
+app.controller('MyController', function($scope) {
   $scope.person = {
-    name: "Guest"
+    name: "Ari Lerner"
   };
+});
+
+app.controller('ParentController', function($scope) {
+  $scope.person = {greeted: false};
+});
+
+app.controller('ChildController', function($scope) {
   $scope.sayHello = function() {
     $scope.person.greeted = true;
-    $scope.person.reply =  'Hi '+$scope.person.name+' . Thank you.';
   }
 });
