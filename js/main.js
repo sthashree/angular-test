2416ba81ed0bab1a810b46febe807eb8d9a8c57b
sshrestha <sshrestha@medforce.net>
2015-07-29 11:55:34 -0400

    Added menu to page

--- js/main.js
+++ js/main.js
@@ -1,16 +1,54 @@
 
 var app = angular.module('myApp', [
+              'ngRoute',
 							'myApp.driverlist',
 							'myApp.studentlist',
   							'myApp.services']);
 
+  app.config(function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl : 'pages/home.html',
+        controller  : 'mainController'
+      })
+      // route for the about page
+      .when('/about', {
+          templateUrl : 'pages/about.html',
+          controller  : 'aboutController'
+      })
+
+      // route for the contact page
+      .when('/contact', {
+          templateUrl : 'pages/contact.html',
+          controller  : 'contactController'
+      });
+  });
+
+  // create the controller and inject Angular's $scope
+      app.controller('mainController', function($scope) {
+          // create a message to display in our view
+          $scope.message = 'Everyone come and see how good I look!';
+      });
+
+      app.controller('aboutController', function($scope) {
+          $scope.message = 'Look! I am an about page.';
+      });
+
+      app.controller('contactController', function($scope) {
+          $scope.message = 'Contact us! JK. This is just a demo.';
+      });
 
 app.controller('greetingController', function($scope) {
   $scope.person = {
     name: "Guest"
   };
-  $scope.sayHello = function() {
+
+  app.controller('menuController', function($scope) {
+
+  });
+$scope.sayHello = function() {
     $scope.person.greeted = true;
     $scope.person.reply =  'Hi '+$scope.person.name+' . Thank you.';
   }
 });
+
